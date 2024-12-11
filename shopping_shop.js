7bb09de5e62a2cd63cec701155afbf8aec7b2952
Ma Boutique <newtechfacile@gmail.com>
2024-12-11 19:58:31 +0000

    Update shopping_shop.js

--- shopping_shop.js
+++ shopping_shop.js
@@ -70,7 +70,7 @@
 
                 const validateTelephone = () => {
                     const telephoneError = document.getElementById('telephoneError');
-                    const telephoneRegex = /^[0-9]{0}$/;
+                    const telephoneRegex = /^[0-9]{}$/;
                     if (!telephoneRegex.test(telephoneInput.value)) {
                         telephoneError.textContent = 'Numéro de téléphone invalide (10 chiffres)';
                         return false;
